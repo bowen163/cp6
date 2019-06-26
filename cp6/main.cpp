f1f69b345af6b6dd5ed6486ed4fccf090b8e3b33
bowen163 <763893371@qq.com>
2019-06-26 20:57:12 +0800

    Initial Commit

---
+++ cp6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cp6
+//
+//  Created by s20181102936 on 2019/6/26.
+//  Copyright © 2019 s20181102936. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
